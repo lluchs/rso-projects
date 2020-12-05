db87259cda54045f98a9bea160938acf1097697b
Lukas Werling <lukas@lwrl.de>
2020-12-05 18:53:22 +0100

    Write Prometheus statistics

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
+	"os"
 	"time"
 
 	"github.com/turnage/graw/reddit"
@@ -36,6 +38,37 @@ func printGanttChartData(posts []reddit.Post) {
 			}
 		}
 	}
+}
+
+var prometheusTemplate = template.Must(template.New("metrics.txt").Parse(`
+# HELP rso_last_run_seconds Timestamp of the last run
+# TYPE rso_last_run_seconds gauge
+rso_last_run_seconds {{.LastRunTS}}
+
+# HELP rso_data_items_total Number of items fetched from Reddit or YouTube
+# TYPE rso_data_items_total gauge
+rso_data_items_total{type="posts"} {{.PostsTotal}}
+rso_data_items_total{type="weekly_updates"} {{.WeeklyUpdatesTotal}}
+rso_data_items_total{type="videos"} {{.VideosTotal}}
+`))
+
+func writePrometheusStats(client *DataClient) error {
+	f, err := os.Create("static/metrics.tmp")
+	if err != nil {
+		return fmt.Errorf("creating static/metrics.tmp failed: %w", err)
+	}
+	err = prometheusTemplate.Execute(f, map[string]interface{}{
+		"LastRunTS":          time.Now().Unix(),
+		"PostsTotal":         len(client.Posts),
+		"WeeklyUpdatesTotal": len(client.WeeklyUpdates),
+		"VideosTotal":        len(client.Videos),
+	})
+	f.Sync()
+	f.Close()
+	if err != nil {
+		return err
+	}
+	return os.Rename("static/metrics.tmp", "static/metrics.txt")
 }
 
 // https://www.reddit.com/r/TheRedditSymphony/search.json?restrict_sr=1&sort=new&q=flair:%22Approved%20Project%22&limit=100
@@ -77,4 +110,8 @@ func main() {
 	//printProjects(&search)
 	//printGanttChartData(&search)
 	createHTMLPage(client)
+
+	if err = writePrometheusStats(client); err != nil {
+		fmt.Printf("failed writing stats: %s\n", err)
+	}
 }
